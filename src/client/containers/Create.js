6be148b70a413765667f6c2627fa0b322fe24389
Sean Smith <sean.smith.2009@gmail.com>
2016-11-19 11:54:09 -0800

    added feature for quiz title

--- src/client/containers/Create.js
+++ src/client/containers/Create.js
@@ -11,16 +11,22 @@ export default class Create extends React.Component {
 		super();
 		this.state = {
 			quiz: {
+				author: '',
 				title: '',
 				questions: []
 			},
-			title: '',
+			title: false,
+			quizTitle: '',
+			questionTitle: '',
 			answers: ['']
 		}
 		this.handleKeyPress = this.handleKeyPress.bind(this);
+		this.setQuizTitle = this.setQuizTitle.bind(this);
 		this.handleInput = this.handleInput.bind(this);
 		this.addAnswerOption = this.addAnswerOption.bind(this);
 		this.removeOption = this.removeOption.bind(this);
+		this.submitAndContinue = this.submitAndContinue.bind(this);
+		this.submitAndReview = this.submitAndReview.bind(this);
 	}
 	handleKeyPress(key) {
 		if (key.keyCode === 9) {
@@ -28,6 +34,16 @@ export default class Create extends React.Component {
 			if (answers[answers.length - 1] !== '') {
 				this.addAnswerOption();
 			}
+		}
+	}
+	setQuizTitle() {
+		const { quiz, quizTitle } = this.state;
+		if (quizTitle !== '') {
+			quiz.title = quizTitle;
+			this.setState({
+				title: true,
+				quiz
+			});
 		}
 	}
 	handleInput(event) {
@@ -52,6 +68,22 @@ export default class Create extends React.Component {
 			this.setState({ answers });
 		}
 	}
+	submitAndContinue() {
+		const { quiz, answers, questionTitle } = this.state;
+		const question = {
+			questionTitle,
+			answers
+		}
+		quiz.questions.push(question);
+		this.setState({
+			quiz,
+			questionTitle: '',
+			answers: ['']
+		});
+	}
+	submitAndReview() {
+
+	}
 	render() {
 		const renderAnswers = this.state.answers.map( (ans, idx) => {
 			const { answers } = this.state;
@@ -73,37 +105,60 @@ export default class Create extends React.Component {
 		})
 		return (
 			<div className = 'createComponent'>
-				<h1>Create a Quiz</h1>
-				<p className = 'subtitle'>You can create a new quiz here. Add as many options as you want, you can always edit them later!</p>
-				<div className = 'createQuizContainer'>
-					<p className = 'inputTitles'>Title:</p>
-					<input
-						type = 'text'
-						name = 'title'
-						placeholder = 'Enter a Title' 
-						value = {this.state.title}
-						onChange = {this.handleInput}
-						className = 'answerTitle' />
-					<p className = 'inputTitles'>Answer Options</p>
-					{renderAnswers}
-					<button
-						className = 'addOption'
-						onClick = {this.addAnswerOption}>
-						Add Another Option
-					</button>
-					<div className = 'proceedWrapper'>
-						<button
-							className = 'proceedBtn'
-							onClick = {this.addAnswerOption}>
-							Submit and Add Another
-						</button>
-						<button
-							className = 'proceedBtn'
-							onClick = {this.addAnswerOption}>
-							Submit and Review
-						</button>
+				
+				{ !this.state.title ? 
+
+					<div>
+						<h1>Create a Quiz</h1>
+						<p className = 'subtitle'>You can create a new quiz here. Add as many options as you want, you can always edit them later!</p>
+						<p className = 'inputTitles'>Enter a Title for Your Quiz:</p>
+						<input
+							type = 'text'
+							name = 'quizTitle'
+							placeholder = 'Enter a Title' 
+							value = {this.state.quizTitle}
+							onChange = {this.handleInput}
+							className = 'questionTitle' />
+							<button
+								className = 'addOption'
+								onClick = {this.setQuizTitle}>
+								Submit Title and Add Questions
+							</button>
 					</div>
-				</div>
+
+					:
+
+					<div className = 'createQuizContainer'>
+						<h1>Add Questions for {this.state.quizTitle}:</h1>
+						<p className = 'inputTitles'>Question:</p>
+						<input
+							type = 'text'
+							name = 'questionTitle'
+							placeholder = 'Enter a Question' 
+							value = {this.state.questionTitle}
+							onChange = {this.handleInput}
+							className = 'questionTitle' />
+						<p className = 'inputTitles'>Answers:</p>
+						{renderAnswers}
+						<button
+							className = 'addOption'
+							onClick = {this.addAnswerOption}>
+							Add Another Option
+						</button>
+						<div className = 'proceedWrapper'>
+							<button
+								className = 'proceedBtn'
+								onClick = {this.submitAndContinue}>
+								Submit and Add Another
+							</button>
+							<button
+								className = 'proceedBtn'
+								onClick = {this.submitAndReview}>
+								Submit and Review
+							</button>
+						</div>
+					</div> }
+
 			</div>
 		);
 	}
