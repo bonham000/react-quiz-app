a9a199baf9f4edb515486c2474a8232c5dfb621a
Sean Smith <sean.smith.2009@gmail.com>
2016-11-19 15:16:30 -0800

    implemented most of edit question review feature

--- src/client/containers/Create.js
+++ src/client/containers/Create.js
@@ -21,7 +21,7 @@ export default class Create extends React.Component {
 			reviewRequest: false,
 			editQuizTitleState: false,
 			editQuizTitle: '',
-			editReview: [],
+			editReview: new Map(),
 			title: false,
 			quizTitle: '',
 			questionTitle: '',
@@ -39,6 +39,12 @@ export default class Create extends React.Component {
 		this.handleEditQuizTitle = this.handleEditQuizTitle.bind(this);
 		this.submitTitleEdit = this.submitTitleEdit.bind(this);
 		this.cancelTitleEdit = this.cancelTitleEdit.bind(this);
+		this.submitQuestionEdit = this.submitQuestionEdit.bind(this);
+		this.cancelQuestionEdit = this.cancelQuestionEdit.bind(this);
+		this.setCorrectAnswerEdit = this.setCorrectAnswerEdit.bind(this);
+		this.handleOptionEdit = this.handleOptionEdit.bind(this);
+		this.removeOptionEdit = this.removeOptionEdit.bind(this);
+		this.addEditOption = this.addEditOption.bind(this);
 		this.saveQuiz = this.saveQuiz.bind(this);
 	}
 	handleKeyPress(key) {
@@ -145,7 +151,59 @@ export default class Create extends React.Component {
 		});
 	}
 	editQuestion(idx) {
-		console.log('editing: ', idx);
+		const { editReview, quiz } = this.state;
+		editReview.set(idx, null);
+		const question = quiz.questions[idx];
+		const questionID = 'editQuestion' + idx;
+		const editQuestionTitleID = 'editQuestionTitle' + idx;
+		this.setState({
+			[questionID]: question,
+			editReview,
+			[editQuestionTitleID]: question.questionTitle
+		});
+	}
+	setCorrectAnswerEdit(questionIdx, answerIdx) {
+		const editQuestionID = 'editQuestion' + questionIdx;
+		const editQuestion = this.state[editQuestionID];
+		editQuestion.correctAnswer = answerIdx;
+		this.setState({
+			[editQuestionID]: editQuestion
+		});
+	}
+	removeOptionEdit(questionIdx, answerIdx) {
+		const editQuestionID = 'editQuestion' + questionIdx;
+		const editQuestion = this.state[editQuestionID];
+		if (editQuestion.answers.length > 1) {
+			editQuestion.answers.splice(answerIdx, 1);
+			this.setState({
+				[editQuestionID]: editQuestion
+			});
+		}
+	}
+	handleOptionEdit(questionIdx, answerIdx, event) {
+		const editQuestionID = 'editQuestion' + questionIdx;
+		const editQuestion = this.state[editQuestionID];
+		editQuestion.answers[answerIdx] = event.target.value;
+		this.setState({
+			[editQuestionID]: editQuestion
+		});
+	}
+	addEditOption(idx) {
+		const editQuestionID = 'editQuestion' + idx;
+		const editQuestion = this.state[editQuestionID];
+		editQuestion.answers.push('');
+		console.log(editQuestion)
+		this.setState({
+			[editQuestionID]: editQuestion
+		});
+	}
+	submitQuestionEdit() {
+
+	}
+	cancelQuestionEdit(idx) {
+		const { editReview } = this.state;
+		editReview.delete(idx);
+		this.setState({ editReview });
 	}
 	saveQuiz() {
 		console.log('dispatching quiz saving action here');
@@ -260,46 +318,95 @@ export default class Create extends React.Component {
 								:
 
 								<div>
+									<h2>Edit Quiz Title:</h2>
 									<input
 									type = 'text'
 									name = 'editQuizTitle'
 									value = {this.state.editQuizTitle}
 									onChange = {this.handleInput} /> 
-									<button onClick = {this.submitTitleEdit}className = 'proceedBtn'>Submit Title Edit</button>
-									<button onClick = {this.cancelTitleEdit}className = 'proceedBtn'>Cancel Edit</button>
+									<button onClick = {this.submitTitleEdit}className = 'editBtn'>Submit Title Edit</button>
+									<button onClick = {this.cancelTitleEdit}className = 'editBtn'>Cancel Edit</button>
 								</div> }
 
 							{this.state.quiz.questions.map( (question, idx) => {
+								const { editReview } = this.state;
 								const { correctAnswer } = question;
-								return (
-									<div key = {idx}>
-										<p className = 'inputTitles'>Question {idx + 1}:</p>
-										<h2 className = 'questionTitleReview'>
-											<i
-												onClick = {this.editQuestion.bind(this, idx)}
-												className = "fa fa-pencil-square-o fa-editQuestion"
-												aria-hidden = "true">
-											</i>
-											{question.questionTitle}
-										</h2>
-										<p className = 'inputTitles'>Answers:</p>
-										{question.answers.map( (answer, index) => {
-											let style = {
-												background: 'rgba(225,225,225,0.5)'
-											}
-											if (correctAnswer === index) {
-												style = {
-													background: '#06C1FF'
+								if (editReview.has(idx)) {
+									return (
+										<div key = {idx}>
+											<h2>Editing {question.questionTitle}</h2>
+											<p className = 'inputTitles'>Edit Question Title:</p>
+											<input
+												type = 'text'
+												name = {'editQuestionTitle' + idx}
+												value = {this.state['editQuestionTitle' + idx]}
+												onChange = {this.handleInput} />
+											<p className = 'inputTitles'>Edit Question Answers:</p>
+											{ question.answers.map( (answer, index) => {
+												const questionID = 'editQuestion' + idx;
+												return (
+													<div className = 'answerContainer' key = {index}>
+
+														{ this.state[questionID].correctAnswer === index ?
+														
+															<i className = "fa fa-check-box fa-check-square-o" aria-hidden = "true"></i>
+
+														:
+
+															<i
+																id = 'checkBox'
+																className = "fa fa-check-box fa-square-o"
+																aria-hidden = "true"
+																onClick = {this.setCorrectAnswerEdit.bind(this, idx, index)}></i> }
+
+														<input
+															type = 'text'
+															value = {this.state[questionID].answers[index]}
+															onChange = {this.handleOptionEdit.bind(this, idx, index)}
+															className = 'answerInput' />
+														<i
+															onClick = {this.removeOptionEdit.bind(this, idx, index)}
+															className = "fa fa-times"
+															aria-hidden = "true"></i>
+													</div>
+												);
+											})}
+											<button onClick = {this.addEditOption.bind(this, idx)} className = 'editBtn'>Add Another Option</button>
+											<button onClick = {this.submitQuestionEdit.bind(this, idx)} className = 'editBtn'>Submit Edit</button>
+											<button onClick = {this.cancelQuestionEdit.bind(this, idx)} className = 'editBtn'>Cancel Edit</button>
+										</div>
+										)
+								} else {
+									return (
+										<div key = {idx}>
+											<p className = 'inputTitles'>Question {idx + 1}:</p>
+											<h2 className = 'questionTitleReview'>
+												<i
+													onClick = {this.editQuestion.bind(this, idx)}
+													className = "fa fa-pencil-square-o fa-editQuestion"
+													aria-hidden = "true">
+												</i>
+												{question.questionTitle}
+											</h2>
+											<p className = 'inputTitles'>Answers:</p>
+											{question.answers.map( (answer, index) => {
+												let style = {
+													background: 'rgba(225,225,225,0.5)'
 												}
-											}
-											return (
-												<div className = 'answerReview' key = {index} style = {style}>
-													<p>{answer}</p>
-												</div>
-											);
-										}) }
-									</div>
-								);
+												if (correctAnswer === index) {
+													style = {
+														background: '#06C1FF'
+													}
+												}
+												return (
+													<div className = 'answerReview' key = {index} style = {style}>
+														<p>{answer}</p>
+													</div>
+												);
+											}) }
+										</div>
+									);
+								}
 							}) }
 							<button className = 'saveBtn' onClick = {this.saveQuiz}>Save and Submit Your Quiz</button>
 						</div>
