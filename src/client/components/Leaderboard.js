90809d000afecb446c7f3d2ac66ebdf4df246f8a
Sean Smith <sean.smith.2009@gmail.com>
2016-11-19 22:19:40 -0800

    created leaderboard sort feature

--- src/client/components/Leaderboard.js
+++ src/client/components/Leaderboard.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
+@connect(
+	state => ({
+		leaders: state.leaderboard
+	}),
+	null
+)
 export default class Leaderboard extends React.Component {
 	render() {
+		const { leaders } = this.props
+		const mapLeaders = leaders.map( (board, index) => {
+			return (
+				<div key = {index}>
+					<h1>Quiz: {board.quiz}</h1>
+					{board.leaders.sort( (a,b) => b.score - a.score ).map( (leader, idx) => {
+						return (
+							<div key = {idx}>
+								<h1>User: {leader.user} | Score: {leader.score}</h1>
+							</div>
+						)
+					}) }
+				</div>
+			);
+		})
 		return (
 			<div>
-				<h1>Leaderboard Component</h1>
+				<h1>Leaderboard</h1>
+				{mapLeaders}
 			</div>
 		);
 	}
