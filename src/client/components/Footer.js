49380a7ee60edf5cec108d930de6688ea2bb3723
Sean Smith <sean.smith.2009@gmail.com>
2016-11-20 13:49:32 -0800

    updated technologies in footer

--- src/client/components/Footer.js
+++ src/client/components/Footer.js
@@ -5,7 +5,7 @@ export default class Footer extends React.Component {
 	render() {
 		return (
 			<div className = 'footer'>
-				<p><a target = "_blank" href = "https://forum.freecodecamp.com/t/computer-frontend-web-development-challenge-november-16-to-26/55986">This was created as a study experiment for campers at Free Code Camp</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a target = "_blank" href = "https://github.com/bonham000/react-quiz-app">View the source on GitHub</a></p>
+				<p><a target = "_blank" href = "https://forum.freecodecamp.com/t/computer-frontend-web-development-challenge-november-16-to-26/55986">This was created as a study experiment for campers at Free Code Camp using React, Redux, and Node</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a target = "_blank" href = "https://github.com/bonham000/react-quiz-app">View the source on GitHub</a></p>
 			</div>
 		);
 	}
