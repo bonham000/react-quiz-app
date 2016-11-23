e6ba1db455eb7722dbb03abd4b99542da3b95245
Sean Smith <sean.smith.2009@gmail.com>
2016-11-23 00:50:54 -0800

    fixed bug in leaderboards

--- dist/server/models/leaderboard.js
+++ dist/server/models/leaderboard.js
@@ -13,6 +13,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 var Schema = _mongoose2.default.Schema;
 
 var Leaderboard = new Schema({
+	id: String,
 	quiz: String,
 	leaders: Array
 });
--- src/server/models/leaderboard.js
+++ src/server/models/leaderboard.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema;
 
 const Leaderboard = new Schema({
+	id: String,
 	quiz: String,
 	leaders: Array
 });
