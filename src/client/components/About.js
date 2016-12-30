95a307af6e9c9701ef5bd6d1bd17c5b80318b2e3
Sean Smith <sean.smith.2009@gmail.com>
2016-12-30 14:04:22 -0800

    updated project to Quiz-it

--- src/client/components/About.js
+++ src/client/components/About.js
@@ -20,23 +20,18 @@ class About extends React.Component {
 		  <div className = 'aboutWrapper'>
 
 				{ this.props.isAuthenticated ? 
-		    <h1>Welcome to the React Quiz App {this.props.user}!</h1> :
-		    <h1>Welcome to the React Quiz App!</h1> }
+		    <h1>Welcome to <b>Quiz-it</b> {this.props.user}!</h1> :
+		    <h1>Welcome to <b>Quiz-it</b></h1> }
 
 		    { !this.props.isAuthenticated && 
 					<div className = 'description'>
-						<p>You can go ahead and <Link to = 'quiz'>take the Quiz</Link> to test your knowledge of React, or login with GitHub to create your own quizzes. If you want your score to be recorded on the leaderboard, be sure to login.</p>
+						<p>You can go ahead and view <Link to = 'quiz'>existing quizzes</Link> or login with GitHub to create your own quizzes. If you want your score to be recorded on the leaderboard, be sure to login before taking the quiz. Enjoy! <i className="em em-smile"></i></p>
 					</div> }
 
 				{ this.props.isAuthenticated &&
 					<div className = 'description'>
 						<p>This app will help you learn React and Redux, awesome new cutting edge web technologies! Feel free to go ahead and <Link to = 'quiz'>take the Quiz</Link> to test your React skills, or you can just use this app to <Link to = 'create'>create your own quizzes</Link> on any topic! Enjoy!</p>
 					</div> }
-
-					<div className = 'logos'>
-						<a target = '_blank' href = 'https://facebook.github.io/react/'><img src = {reactLogo} alt = "React Logo"/></a>
-						<a target = '_blank' href = 'http://redux.js.org/'><img src = {reduxLogo} alt = "Redux Logo"/></a>
-					</div>
 
 		  </div>
 	  );
